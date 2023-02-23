bc205624caab39ecc3865f39dddd721391d2b231
kritsanapr <kritsana.pr.41@gmail.com>
2023-02-23 17:44:33 +0700

    [Add] Add .gitignore file in work space

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3333/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
   return (
-    <form className="register" action="">
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+    <form className="register" onSubmit={register}>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Register</button>
     </form>
   );
